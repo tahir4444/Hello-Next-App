634bc1e2408c7234df3a1f142e2003320257c91f
TAHIR <tahir4444@gmail.com>
2025-06-09 17:41:15 +0530

    first commit button updated

--- components/click-me-button.tsx
+++ components/click-me-button.tsx
@@ -7,10 +7,12 @@ export default function ClickMeButton() {
 
   return (
     <button
-      onClick={handleClick}
-      className="bg-blue-500 text-white p-2 rounded"
+      onClick={() => alert('Button clicked!')}
+      type="button"
+      className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors duration-200 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+      onMouseOver={() => console.log('Mouse over button')}
     >
-      Click Me
+      Click Me On!
     </button>
   );
 }
